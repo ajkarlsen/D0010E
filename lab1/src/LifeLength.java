4a6b16328ca336699135bcc8a99e306abd846f1d
Alex Karlsen <alex.j.karlsen@gmail.com>
2025-01-21 16:58:27 +0100

    Task 4 done

--- lab1/src/LifeLength.java
+++ lab1/src/LifeLength.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class LifeLength {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) { //Task 1,2
         while (true) {
             int a0;
             Scanner scan = new Scanner(System.in);
@@ -26,8 +26,38 @@ public class LifeLength {
             }
 
         }
+    } */
 
+    /*public static void main(String[] args) { // Task 3
+        while (true) {
+            int a0;
+            int n;
+            Scanner scan = new Scanner(System.in);
+            try {
+                System.out.print("a0: ");
+                a0 = scan.nextInt();
+                if (a0 == 0) {
+                    scan.close();
+                    break;
+                } else {
+                    System.out.print("n: ");
+                    n = scan.nextInt();
+                    System.out.println(iterateF(a0, n));
+                }
+
+            }
+            catch (Exception e) {
+                System.out.println("Fel: Ej ett heltal > 0");
+            }
+        }
+    } */
+
+    public static void main(String[] args) {
+        for (int a0 = 1; a0 <= 15; a0++) {
+            System.out.println(intsToString(a0, iterLifeLength(a0)));
+        }
     }
+
     public static int iterLifeLength(int a0) {
         int c = 1;
         while (a0 != 1) {
@@ -35,6 +65,11 @@ public class LifeLength {
             c++;
         } return c;
     }
+
+    public static String intsToString(int a0, int len) {
+        return "The life length of " + a0 + " is " + len + ".";
+    }
+
     public static int iterateF(int a0, int n) {
         for (int i = 0; i <= n; i++) {
             a0 = f1(a0);
