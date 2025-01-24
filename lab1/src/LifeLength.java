bc634826e9fdc517fccc3a465806fc86439564f3
Alex Karlsen <alex.j.karlsen@gmail.com>
2025-01-24 13:59:45 +0100

    Lab1 redovisad

--- lab1/src/LifeLength.java
+++ lab1/src/LifeLength.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-/* Det hade lett till att programet hade loopat för alltid
-Detta för att min while-loop körs tills att a0 blir 1 vilket aldrig kommer att hända för ett sådant tal.
+/* Task 7: Det hade lett till att programet hade loopat för alltid
+Detta är pga att min funktion är rekursiv och kommer att fortsätta rekurreras tills att vi får a0 = 1.
+Om vi aldrig får a0 = 1 komemr det att leda till att programmet fortsätter kalla på funktionen och aldrig slutar.
+
 Det skulle alltså aldrig terminera
 */
+
+
+//Task 5
+// 1.   l(x) = l(f(x)) + 1, (x != 1)
+// 2.   l(x) = 0,           (x == 1)
+
 
 
 public class LifeLength {
 
     public static void main(String[] args) {
-        int n = 4;
+        int n = 2;
 
         switch (n) {
             case 1: task1(); break;
@@ -54,12 +62,8 @@ public class LifeLength {
                 if (a0 == 0) {
                     break;
                 } else {
-                    System.out.println("f(" + a0 + ") = " + f1(a0));
-                    System.out.println("f2(" + a0 + ") = " + f2(a0));
-                    System.out.println("f4(" + a0 + ") = " + f4(a0));
-                    System.out.println("f8(" + a0 + ") = " + f8(a0));
-                    System.out.println("f16(" + a0 + ") = " + f16(a0));
-                    System.out.println("f32(" + a0 + ") = " + f32(a0));
+                    System.out.println("f(" + a0 + ")=" + f1(a0) + "    f2(" + a0 + ") = " + f2(a0) + "     f4(" + a0 + ") = " + f4(a0) + "     f8(" + a0 + ") = " + f8(a0) + "     f16(" + a0 + ") = " + f16(a0) + "   f32(" + a0 + ") = " + f32(a0));
+
                 }
             } catch (Exception e) {
                 scan.nextLine();
@@ -70,15 +74,16 @@ public class LifeLength {
         scan.close();
     }
 
-    public static void task4() {
-        //Task 5, vi ser att l(x) = l(f(x)) + 1 för x!=1, för
-        //x = 1 får vi bara l(x) = 0
+    public static int iterLifeLength(int a0) {
+        int c = 0;
+        while (a0 != 1) {
+            a0 = f1(a0);
+            c++;
+        } return c;
+    }
 
-        for (int a0 = 1; a0 <= 15; a0++) {
-            System.out.println(intsToString(a0, iterLifeLength(a0)));
-            // Task 5:
-            //System.out.println(a0 + ", " + iterLifeLength(a0) + ", " + iterLifeLength(f1(a0)));
-        }
+    public static String intsToString(int a0, int len) {
+        return "The life length of " + a0 + " is " + len + ".";
     }
 
 
@@ -106,9 +111,23 @@ public class LifeLength {
         }
     }
 
+    public static int iterateF(int a0, int n) {
+        for (int i = 0; i < n; i++) {
+            a0 = f1(a0);
+        }
+        return a0;
+    }
+
+    public static void task4() {
+
+        for (int a0 = 1; a0 <= 15; a0++) {
+            System.out.println(intsToString(a0, iterLifeLength(a0)));
+        }
+    }
+
     public static void task6() {
         for (int a0 = 1; a0 <= 15; a0++) {
-            System.out.println(a0 + ", " + iterLifeLength(a0) + ", " + recLifeLength(a0));
+            System.out.println("Lifelength for " + a0 + ": " + iterLifeLength(a0) + ", " + recLifeLength(a0));
         }
     }
 
@@ -118,25 +137,6 @@ public class LifeLength {
         } else {
             return iterLifeLength((f1(a0))) + 1;
         }
-    }
-
-    public static int iterLifeLength(int a0) {
-        int c = 0;
-        while (a0 != 1) {
-            a0 = f1(a0);
-            c++;
-        } return c;
-    }
-
-    public static String intsToString(int a0, int len) {
-        return "The life length of " + a0 + " is " + len + ".";
-    }
-
-    public static int iterateF(int a0, int n) {
-        for (int i = 0; i <= n; i++) {
-            a0 = f1(a0);
-        }
-        return a0;
     }
 
     public static int f1(int a0) {
--- lab1/src/Raise.java
+++ lab1/src/Raise.java
@@ -2,34 +2,51 @@ public class Raise {
     public static int c1 = 0;
     public static int c2 = 0;
 
+    /* Task 10
+    Storleken på k kommer att påverka mest då det är pga den som funktionen rekurreras såpass många gånger.
+    Den ska alltid ner till 0, antingen genom att halveras eller ta -1 på så desto större den är desto längre
+    tid kommer funktionen att ta. Storleken på x har ingen betydelse då det där bara är en multiplikation
+    */
+
+    //Recraiseone
+    //Vi ser relativt tydligt att denna kommer att representeras av en rät linje
+    //Detta är tydligt då den alltid tar -1 och kommer således alltid att få k stycker rekursioner
+    // N(k) = k + 1, +1 är för att vi börjar med 0
+
+    //Recraisehalf
+    //Här inser vi att vi kommer att få en logaritmisk mängd rekursioner
+    //Detta är då den konstant delas med två och vi får alltså en betydligt snabbare algoritm
+    //Ser ut exakt som N(k) = (log2(k) + 1) ceiling
+
     public static void main(String[] args) {
-        /*System.out.println("Använder recRaiseOne:");
+/*
+        System.out.println("Använder recRaiseOne:");
         for (int i = 1; i <= 15; i++) {
-            System.out.println("1.5^" + i + "=" + recRaiseOne(1.5, i) + ", Antal rekursioner = " + Raise.c1);
+            System.out.println("1.5^" + i + "=" + recRaiseOne(1.5, i) + ", Antal rekursioner = " + c1);
+            c1 = 0;
         }
-        System.out.println(" ");
         System.out.println("Använder recRaiseHalf:");
         for (int i = 1; i <= 15; i++) {
-            System.out.println("1.5^" + i + "=" + recRaiseHalf(1.5, i) + ", Antal rekursioner = " + Raise.c2);
+            System.out.println("1.5^" + i + "=" + recRaiseHalf(1.5, i) + ", Antal rekursioner = " + c2);
+            c2 = 0;
         } */
+
         for (int i = 0; i <= 10000; i++) {
             c1 = 0;
             recRaiseOne(1.0005, i);
-            //System.out.print(c1 + ",");
+            System.out.println(i + ":" + c1);
         }
         for (int i = 0; i <= 10000; i++) {
             c2 = 0;
             recRaiseHalf(1.0005, i);
-            System.out.print(c2 + ",");
+            System.out.println(i + ":" + c2);
         }
     }
 
 
-    //Vi ser relativt tydligt att denna kommer att representeras av en rät linje
-    //Detta är tydligt då den alltid tar -1 och kommer således alltid att få k stycker rekursioner
-    // N(k) = k + 1, +1 är för att vi börjar med 0
+
     public static double recRaiseOne(double x, int k) {
-        Raise.c1++;
+        c1++;
         if (k == 0) {
             return 1;
         } else {
@@ -37,11 +54,9 @@ public class Raise {
         }
     }
 
-    //Här inser vi att vi kommer att få en logaritmisk mängd rekursioner
-    //Detta är då den konstant delas med två och vi får alltså en betydligt snabbare algoritm
-    //Ser ut exakt som N(k) = (log2(k) + 1) ceiling
+
     public static double recRaiseHalf(double x, int k) {
-        Raise.c2++;
+        c2++;
         if (k == 0) {
             return 1;
         } else if (k % 2 == 0) {
@@ -56,8 +71,3 @@ public class Raise {
 
 }
 
-/* Task 10
-Storleken på k kommer att påverka mest då det är pga den som funktionen rekurreras såpass många gånger.
-Den ska alltid ner till 0, antingen genom att halveras eller ta -1 på så desto större den är desto längre
-tid kommer funktionen att ta.
- */
