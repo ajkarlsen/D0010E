8fc25882ea3b4558fbd9b1fa1a4f0096fa9ba684
Alex Karlsen <alex.j.karlsen@gmail.com>
2025-01-30 16:37:53 +0100

    Lab 2 progress

--- lab2/src/MyArrayList.java
+++ lab2/src/MyArrayList.java
@@ -13,24 +13,31 @@ import java.util.function.UnaryOperator;
 @SuppressWarnings("serial")
 public class MyArrayList<E> implements Serializable, Cloneable, Iterable<E>,
 		Collection<E>, List<E>, RandomAccess {
+		private E[] array;
 
     	// ---------------------------------------------------------------
 
 	public static void main(String[] args) {
-		MyArrayList<String> strlist = new MyArrayList<String>();
-		
+		MyArrayList<String> strlist = new MyArrayList<String>(4);
 		// testa metoder härifrån
-		
+		strlist.add(1, "A");
+		strlist.add(1, "B");
+		strlist.MyArrayList();
 	}
 
     	// ---------------------------------------------------------------
     
 	public MyArrayList(int initialCapacity) {
 		/* ska implementeras */
+		array = (E[]) new Object[initialCapacity];
 	}
 
-	public MyArrayList() {
+	public void MyArrayList() {
 		/* ska implementeras */
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		System.out.println(array[3]);
 	}
 
 	// -- 1 --
@@ -38,35 +45,71 @@ public class MyArrayList<E> implements Serializable, Cloneable, Iterable<E>,
 	@Override
 	public int size() {
 		/* ska implementeras */
-	    return -1; /* bara med för att Eclipse inte ska klaga */
+		int storlek = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				storlek++;
+			}
+		}
+		return storlek;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		/* ska implementeras */
-		return false; /* bara med för att Eclipse inte ska klaga */
+		if (this.size() == 0) {
+			return true;
+		} else {
+			return false; /* bara med för att Eclipse inte ska klaga */
 	}
-
+}
 	@Override
 	public void clear() {
 		/* ska implementeras */
+		for (int i = 0; i < array.length; i++) {
+			array[i] = null;
+		}
 	}
 
 	// -- 2 --
 
 	public void ensureCapacity(int minCapacity) {
 		/* ska implementeras */
+		if (minCapacity > array.length) {
+			int nyKap = array.length * 2;
+			E[] newArray = (E[]) new Object[nyKap];
+
+			for (int i = 0; i < this.size(); i++) {
+				newArray[i] = this.array[i];
+			}
+			this.array = newArray;
+		}
 	}
 
 	public void trimToSize() {
 		/* ska implementeras */
+		int nyKap = this.size();
+		E[] newArray = (E[]) new Object[nyKap];
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != null) {
+				newArray[i] = array[i];
+			}
+		}
+		this.array = newArray;
 	}
     
 	// -- 3 --
     
 	@Override
 	public void add(int index, E element) {
-		/* ska implementeras */
+		this.ensureCapacity(this.size() + 1);
+
+		if (array[index] == null) {
+			array[index] = element;
+		} else {
+			array[index+1] = array[index];
+		}
+		array[index] = element;
 	}
 
 	@Override
