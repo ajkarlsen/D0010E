b721bfdcc935f3373f1c4f9b86df799ea99e2572
Alex Karlsen <alex.j.karlsen@gmail.com>
2025-02-20 12:39:10 +0100

    Struktur klar

--- lab4/src/FIFO.java
+++ lab4/src/FIFO.java
@@ -1,4 +1,50 @@
-public class FIFO {
-    private Object[] queue;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
+public class FIFO implements Queue {
+    private ArrayList<Object> elements;
+    private int maxSize;
+
+    public FIFO() {
+        elements = new ArrayList<>();
+        maxSize = 0;
+    }
+
+    @Override
+    public void add(Object item) {
+    }
+
+    @Override
+    public void removeFirst() {
+    }
+
+    @Override
+    public Object first() {
+        return elements.getFirst();
+    }
+
+    @Override
+    public int maxSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object f) {
+        return false;
+    }
 }
